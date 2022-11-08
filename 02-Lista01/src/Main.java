d1b82a0252ba0f37ce54c2fe0899a202f82494fb
Marcelo Gonçalves de Souza <mgoncalves@digitalhouse.com>
2022-11-08 19:53:26 -0300

    Update

--- 02-Lista01/src/Main.java
+++ 02-Lista01/src/Main.java
@@ -136,17 +136,97 @@ public class Main {
         média salarial dos mesmos.
         */
 
-        double salario, mediaSalarial, acumulador = 0.0; // padrão camelcase
+//        double salario, mediaSalarial, acumulador = 0.0; // padrão camelcase
+//
+//        for (int i = 1; i <= 15; i++) {
+//            System.out.print("Digite o " + i + "º salário: "); // Atl + 0186
+//            salario = sc.nextDouble();
+//            acumulador += salario; // acumulador = acumulador + salario;
+//        }
+//
+//        mediaSalarial = acumulador / 15;
+//        System.out.printf("A média dos salários dos funcionários é de: %.2f", mediaSalarial);
 
-        for (int i = 1; i <= 15; i++) {
-            System.out.print("Digite o " + i + "º salário: "); // Atl + 0186
-            salario = sc.nextDouble();
-            acumulador += salario; // acumulador = acumulador + salario;
+        /* 1.5.1
+        Construa um algoritmo que contenha um array com todas as alíquotas do
+        Imposto de Renda de Pessoa Física, são elas {0.0, 7.5, 15.0, 22.5, 27.5}.
+        Logo após os imprima no console.
+        */
+
+//        double[] aliquotas = {0.0, 7.5, 15.0, 22.5, 27.5}; // 0 1 2 3 4
+//
+//        for (double aliq : aliquotas) {
+//            System.out.println(aliq + "%");
+//        }
+
+        /* 1.6.1
+        Construa um algoritmo que receba um salário bruto de um funcionário e envie
+        para uma função que calcule em qual alíquota de Imposto de Renda de Pessoa Física
+        o mesmo estará enquadrado com base nos dados abaixo:
+
+        até R$ 1.903,98 – Isento
+        de R$ 1.903,99 a R$ 2.826,65 – 7,5%
+        de R$ 2.826,66 a R$ 3.751,05 – 15.0%
+        de R$ 3.751,06 a R$ 4.664,68 – 22,5%
+        acima de R$ 4.664,68 – 27,5%
+        */
+
+//        double salario; // Escopo global
+//
+//        System.out.print("Digite o salário bruto do funcionário: ");
+//        salario = sc.nextDouble(); // 3000.0
+
+        // Função ou método
+//        calculaImposto(salario);
+        // O parâmetro salario é enviado para a função
+
+        /* 1.7.1
+        Construa um algoritmo que possua um array de 5 (cinco posições). Logo após
+        adicione as seguintes marcas: {“Ford”, “Chevrolet”, “Volkswagen”, “Fiat”, “Renault”}.
+
+        Depois busque dentro do vetor todas as palavras (pelas suas posições) e conte
+        quantas letras tem cada uma delas. Ao final imprima na tela qual delas possui a
+        maior quantidade de letras e quantas elas são.
+        */
+
+        String[] marcas = new String[5];
+        marcas[0] = "Ford";
+        marcas[1] = "Chevrolet";
+        marcas[2] = "Volkswagen";
+        marcas[3] = "Fiat";
+        marcas[4] = "Renault";
+
+        int maior = 0, indice = 0;
+
+        for (int i = 0; i <= 4; i++) {
+            int qtdeLetras = marcas[i].length(); // 4 9 10 4 7
+            //System.out.println(qtdeLetras);
+            if (qtdeLetras > maior) {
+                maior = qtdeLetras; // 10
+                indice = i; // 2
+            }
         }
 
-        mediaSalarial = acumulador / 15;
-        System.out.printf("A média dos salários dos funcionários é de: %.2f", mediaSalarial);
+        System.out.println("A marca com a maior quantidade de letras é a "
+                            + marcas[indice] + ".");
+        System.out.println("Ela possui " + maior + " letras.");
 
         sc.close();
     }
+
+    public static void calculaImposto(double sal) { // Escopo local
+        if (sal <= 1903.98) {
+            System.out.println("O contribuinte está isento de IR.");
+            System.out.println("Será descontado 6% de vale-transporte deste trabalhador.");
+        } else if (sal <= 2826.65) {
+            System.out.println("Aplicar a alíquota de 7.5%.");
+        } else if (sal <= 3751.05) {
+            System.out.println("Aplicar a alíquota de 15.0%.");
+        } else if (sal <= 4664.68) {
+            System.out.println("Aplicar a alíquota de 22.5%.");
+        } else {
+            System.out.println("Aplicar a alíquota de 27.5%.");
+        }
+    }
+
 }
