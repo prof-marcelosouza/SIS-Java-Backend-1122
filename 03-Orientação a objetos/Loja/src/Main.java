b261b41daa656a72760e4ad38214a6de59c074c5
Marcelo Gonçalves de Souza <mgoncalves@digitalhouse.com>
2022-11-09 20:40:47 -0300

    Update

--- 03-Orientação a objetos/Loja/src/Main.java
+++ 03-Orientação a objetos/Loja/src/Main.java
@@ -6,23 +6,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Produto p1 = new Produto();
-        p1.id = 1;
-        p1.nome = "Motorola G8";
-        p1.desc = "Celular de última geração";
-        p1.valor = 980.00;
-        p1.estoque = 25;
-        p1.dataCadastro = Instant.now();
+        Produto p1 = new Produto(1, "Motorola G8", "Celular de última geração");
 
         System.out.println(p1);
 
-        Produto p2 = new Produto();
-        p2.id = 2;
-        p2.nome = "Motorola G9";
-        p2.desc = "Celular de última geração";
-        p2.valor = 1190.00;
-        p2.estoque = 40;
-        p2.dataCadastro = Instant.now();
+        Produto p2 = new Produto(2, "Motorola G9", "Celular de última geração", 1190.00, 40, Instant.now());
+
         System.out.println(p2);
+
+        Produto p3 = new Produto();
+        p3.setId(3);
+        p3.setNome("Moto G");
+        p3.setDesc("Depreciado");
+        p3.setValor(552.0);
+        p3.setEstoque(10);
+        p3.setDataCadastro(Instant.now());
+
+        System.out.println("Impressão do P3:");
+        System.out.println("Id: " + p3.getId());
+        System.out.println("Nome: " + p3.getNome());
+        System.out.println("Desc: " + p3.getDesc());
+        System.out.println("Valor: " + p3.getValor());
+        System.out.println("Estoque: " + p3.getEstoque());
+        System.out.println("Data de cadastro: " + p3.getDataCadastro());
+
     }
 }
