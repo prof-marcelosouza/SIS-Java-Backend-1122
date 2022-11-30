38be18616b8110c338c88d1121572475d6ff0d8e
Marcelo Gonçalves de Souza <mgoncalves@digitalhouse.com>
2022-11-29 21:26:18 -0300

    Update

---
+++ 14-Spring/firstProgram/src/test/java/br/com/sisnema/firstProgram/FirstProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sisnema.firstProgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
