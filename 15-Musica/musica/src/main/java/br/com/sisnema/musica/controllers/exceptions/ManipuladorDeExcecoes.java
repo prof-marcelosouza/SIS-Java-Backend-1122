0d9a532c36d1d1de899b43d4c0acd3063c9b9935
Marcelo Gonçalves de Souza <mgoncalves@digitalhouse.com>
2022-12-15 21:14:48 -0300

    Update

--- 15-Musica/musica/src/main/java/br/com/sisnema/musica/controllers/exceptions/ManipuladorDeExcecoes.java
+++ 15-Musica/musica/src/main/java/br/com/sisnema/musica/controllers/exceptions/ManipuladorDeExcecoes.java
@@ -1,5 +1,6 @@
 package br.com.sisnema.musica.controllers.exceptions;
 
+import br.com.sisnema.musica.services.exceptions.IntegridadeBD;
 import br.com.sisnema.musica.services.exceptions.RecursoNaoEncontrado;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,19 @@ public class ManipuladorDeExcecoes {
         ErroPadrao erro = new ErroPadrao();
         erro.setTimestamp(Instant.now());
         erro.setStatus(status.value()); // 404
-        erro.setError("ERRO NA REQUISIÇÃO - 404!");
+        erro.setError("ERRO NA REQUISIÇÃO (ID não encontrado) - 404!");
+        erro.setMessage(e.getMessage()); // Erro da camada do Service
+        erro.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(erro);
+    }
+
+    @ExceptionHandler(IntegridadeBD.class)
+    public ResponseEntity<ErroPadrao> integridadeDB(IntegridadeBD e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ErroPadrao erro = new ErroPadrao();
+        erro.setTimestamp(Instant.now());
+        erro.setStatus(status.value()); // 400
+        erro.setError("ERRO NA REQUISIÇÃO (Integridade do BD) - 400!");
         erro.setMessage(e.getMessage()); // Erro da camada do Service
         erro.setPath(request.getRequestURI());
         return ResponseEntity.status(status).body(erro);
---
+++ copaQatar2022/src/main/java/br/com/sisnema/copaQatar2022/entities/Patrocinador.java
@@ -0,0 +1,4 @@
+package br.com.sisnema.copaQatar2022.entities;
+
+public class Patrocinador {
+}
